808d81790db22d1954e8ebb848a9cc4e55fdaafd
viennarose <viennapepito01@gmail.com>
2021-10-27 16:32:09 +0800

    Midterm Project

---
+++ app/Http/Controllers/API/UsersController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Rentals;
+use App\Repositories\rentalsRepository;
+use Illuminate\Http\Request;
+use Flash;
+use Response;
+
+class UsersController extends Controller {
+
+    public $successStatus = 200;
+
+    public function rentalsQuery() {
+        $rentals = Rentals::all();
+
+        if (count($rentals) > 0) {
+            return response()->json($rentals, $this->successStatus);
+        } else {
+            return response()->json(['Error' => 'There is no registrations in the database'], 404);
+        }
+    }
+}
+?>
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,8 @@
+<li class="nav-item">
+    <a href="{{ route('rentals.index') }}"
+       class="nav-link {{ Request::is('rentals*') ? 'active' : '' }}">
+        <p>Rentals</p>
+    </a>
+</li>
+
+
